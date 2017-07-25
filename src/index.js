14fed24ef5b5f195f6ca30c1d6f9a0e80acd148f
cristineshipman <cristine.shipman@gmail.com>
2017-07-25 15:31:32 -0500

    class based method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyB-P6kXZgfKpijCcL0evKCQAs-8Mc5f4Vw'
 
 // Creating a new component that will produce HTML.
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 //Take the components generated HTML and render it on the page (in the DOM)
